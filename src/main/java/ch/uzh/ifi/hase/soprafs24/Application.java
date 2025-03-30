3b13d241cef0a22c6c16f4bac4a9dac68f6aaa43
Malai <malaiappan.srinivasan@uzh.ch>
2025-03-30 15:49:11 +0200

    loginValidations-dbConnectionSQL5-logout #60 #61 #62 #63

--- src/main/java/ch/uzh/ifi/hase/soprafs24/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -29,11 +30,16 @@ public class Application {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*") // Allow all origins
+                        .allowedOrigins("*") // Fully open for testing
                         .allowedMethods("*") // Allow all HTTP methods
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true); // Allow credentials
             }
         };
+    }
+
+    @RequestMapping(value = "/**", method = RequestMethod.OPTIONS) // Added Preflight Handler
+    public ResponseEntity<?> handlePreflight() {
+        return ResponseEntity.ok().build();
     }
 }
