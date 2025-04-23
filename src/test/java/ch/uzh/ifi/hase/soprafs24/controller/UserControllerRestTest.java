49e9f75a5d584597c97d3232d409019b698bfbb3
Nacer <mohamednacer.chabbi@uzh.ch>
2025-04-23 23:04:40 +0200

    Bro I want to sleep and added more tests instead

--- src/test/java/ch/uzh/ifi/hase/soprafs24/controller/UserControllerRestTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/controller/UserControllerRestTest.java
@@ -4,11 +4,15 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.server.ResponseStatusException;
 
 import ch.uzh.ifi.hase.soprafs24.entity.User;
 import ch.uzh.ifi.hase.soprafs24.service.OTPService;
@@ -17,13 +21,20 @@ import ch.uzh.ifi.hase.soprafs24.service.UserService;
 @WebMvcTest(UserController.class)
 class UserControllerRestTest {
 
-    @Autowired MockMvc mvc;
-    @MockBean UserService userService;
-    @MockBean OTPService otpService;
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    UserService userService;
+
+    @MockBean
+    OTPService otpService;
 
     @Test
     void getUserById_found() throws Exception {
-        User u = new User(); u.setId(1L); u.setUsername("bob");
+        User u = new User();
+        u.setId(1L);
+        u.setUsername("bob");
         when(userService.getUserById(1L)).thenReturn(u);
 
         mvc.perform(get("/users/1"))
@@ -35,6 +46,38 @@ class UserControllerRestTest {
     void getUserById_notFound() throws Exception {
         when(userService.getUserById(2L)).thenReturn(null);
         mvc.perform(get("/users/2"))
+           .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void searchUsers_withUsernameParam_returnsUsers() throws Exception {
+        User u = new User();
+        u.setId(5L);
+        u.setUsername("alice");
+        when(userService.getUsersByUsername("ali")).thenReturn(List.of(u));
+
+        mvc.perform(get("/users")
+                .param("username", "ali"))
+           .andExpect(status().isOk())
+           .andExpect(jsonPath("$[0].username").value("alice"));
+    }
+
+    @Test
+    void getFriendRequests_validUser_returnsList() throws Exception {
+        when(userService.getFriendRequests(7L)).thenReturn(List.of(2L, 3L));
+
+        mvc.perform(get("/users/7/friendrequests"))
+           .andExpect(status().isOk())
+           .andExpect(jsonPath("$[0]").value(2))
+           .andExpect(jsonPath("$[1]").value(3));
+    }
+
+    @Test
+    void getFriendRequests_userNotFound_returns404() throws Exception {
+        when(userService.getFriendRequests(8L))
+            .thenThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        mvc.perform(get("/users/8/friendrequests"))
            .andExpect(status().isNotFound());
     }
 }
