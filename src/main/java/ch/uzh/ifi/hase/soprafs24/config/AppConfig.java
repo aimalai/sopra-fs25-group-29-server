cb9e654143c41bf2dc514f2fd294a7fd8ee3764e
Admir17 <admir.bjelic@outlook.com>
2025-05-07 21:56:16 +0200

    Error handling for 'No Data' and localStorage.

--- src/main/java/ch/uzh/ifi/hase/soprafs24/config/AppConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/config/AppConfig.java
@@ -3,12 +3,23 @@ package ch.uzh.ifi.hase.soprafs24.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     @Bean
     public RestTemplateBuilder restTemplateBuilder() {
         return new RestTemplateBuilder();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
